e159b8668a7d90da2c069c43204f24e75a6704a9
baral01 <66971391+baral01@users.noreply.github.com>
2022-10-03 18:52:05 +0200

    Corrections. Added better comments.

--- testing/modbus-testing/updating_server.py
+++ testing/modbus-testing/updating_server.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python3
-# pylint: disable=missing-any-param-doc,differing-param-doc
 """Pymodbus Server With Updating Thread.
+Based on this example 
+https://pymodbus.readthedocs.io/en/dev/source/example/updating_server.html
 
-This is an example of having a background thread updating the
-context while the server is operating. This can also be done with
-a python thread::
-
-    from threading import Thread
-    Thread(target=updating_writer, args=(context,)).start()
+This server is used alongside the demo.st code to test if the OpenPLC 
+runtime is working as intended.
+The purpose of this server is to expose two discrete inputs, whose values
+change periodically, to the runtime which will read them and then change
+the value of the output (the coil exposed) according to the behavior 
+written in the demo code.
 """
 import asyncio
 import logging
@@ -21,18 +21,6 @@ from pymodbus.device import ModbusDeviceIdentification
 from pymodbus.server import StartAsyncTcpServer
 from pymodbus.version import version
 
-
-# --------------------------------------------------------------------------- #
-# configure the service logging
-# --------------------------------------------------------------------------- #
-FORMAT = '%(asctime)s %(module)-15s:%(lineno)-8s %(message)s'
-logging.basicConfig(format=FORMAT)
-log = logging.getLogger()
-log.setLevel(logging.DEBUG)
-
-# --------------------------------------------------------------------------- #
-# define your callback process
-# --------------------------------------------------------------------------- #
 class ServerInfo():
     """Simple class to keep track of the server's info outside its creation method."""
     
@@ -58,28 +46,40 @@ class ServerInfo():
     
     def setAddress(self, address):
         self.address = address
-        
 
 
-def updating_writer(extra, cycle):
+# configure the service logging
+FORMAT = '%(asctime)s %(module)-15s:%(lineno)-8s %(message)s'
+logging.basicConfig(format=FORMAT)
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+ 
+def updating_writer(server_context, cycle):
     """Run every so often,
-
     and updates live values of the context. It should be noted
     that there is a lrace condition for the update.
+    Changes the value of the first two discrete inputs based on
+    the current cycle state.
 
-    :param arguments: The input arguments to the call
+    :param server_context: master collection of slave contexts
+    :param cycle: list saving the cycle state (single integer value)
     """
     log.debug("updating the context")
     # extract the slave (it's a single slave context, so first position)
-    context = extra[0]
+    slave_context = server_context[0]
     # Registers: Coils (co), Discrete Inputs (di), Input Registers (ir), Holding Registers (hr)
+    # fc_as_hex mappings
     # di: 2
+    # ir: 4
+    # co: 1, 5, 15
+    # hr: 3, 6, 16, 22, 23
     register = 2
     # only one slave, so id = 0
     slave_id = 0x00
     # first two inputs, so starting point is 0x00
     address = 0x00
-    values = context.getValues(register, address, count=2)
+    values = slave_context.getValues(register, address, count=2)
     txt = f"current values: {str(values)}"
     log.debug(txt)
     state = len(cycle) % 4
@@ -103,16 +103,15 @@ def updating_writer(extra, cycle):
         values = [0, 0]
     txt = f"new values: {str(values)}"
     log.debug(txt)
-    context.setValues(register, address, values)
+    slave_context.setValues(register, address, values)
 
 
 async def run_updating_server(server_info):
-    """Run updating server."""
+    """Run updating server.
     
-
-    # ----------------------------------------------------------------------- #
-    # run the server you want
-    # ----------------------------------------------------------------------- #
+    :param server_info: collection of server's info (context, identity, address)
+    """
+    
     log.debug("Start server")
     await StartAsyncTcpServer(
         context=server_info.getContext(), identity=server_info.getIdentity(), address=server_info.getAddress(), defer_start=False
@@ -121,22 +120,29 @@ async def run_updating_server(server_info):
 
 
 async def main():
-    server_info = ServerInfo()
-    # ----------------------------------------------------------------------- #
-    # initialize your data store
-    # ----------------------------------------------------------------------- #
+
+    # initialize data store
+    # Following the modbus application protocol specification section 4.4
+    # (https://modbus.org/docs/Modbus_Application_Protocol_V1_1b3.pdf)
+    # which states "In a MODBUS PDU each data is addressed from 0 to 65535" and
+    # "In the MODBUS data Model each element within a data block is numbered from 1 to n"
+    # the chosen starting address for the data blocks is "1" with 
+    # ModbusSequentialDataBlock.zero_mode set to "False" (default value).
+    # This means that the first value of the blocks is in position "1".
+    # To change this behavior and have the first value in position "0",
+    # the starting address must be "0" and the "zero_mode" must be enabled.
 
     store = ModbusSlaveContext(
-        di=ModbusSequentialDataBlock(0, [17] * 100),
-        co=ModbusSequentialDataBlock(0, [17] * 100),
-        hr=ModbusSequentialDataBlock(0, [17] * 100),
-        ir=ModbusSequentialDataBlock(0, [17] * 100),
+        di=ModbusSequentialDataBlock(1, [17] * 100),
+        co=ModbusSequentialDataBlock(1, [17] * 100),
+        hr=ModbusSequentialDataBlock(1, [17] * 100),
+        ir=ModbusSequentialDataBlock(1, [17] * 100),
     )
+    
+    # create server context, single slave
     context = ModbusServerContext(slaves=store, single=True)
 
-    # ----------------------------------------------------------------------- #
     # initialize the server information
-    # ----------------------------------------------------------------------- #
     identity = ModbusDeviceIdentification(
         info_name={
             "VendorName": "pymodbus",
@@ -147,7 +153,9 @@ async def main():
             "MajorMinorRevision": version.short(),
         }
     )
-        
+    
+    # save context and identity in a ServerInfo object
+    server_info = ServerInfo()    
     server_info.setContext(context)
     server_info.setIdentity(identity)
     cycle = []
